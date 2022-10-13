e655e641800b95878e3b27994ed3c10032130e2a
Manoj Asarsa <manoj@heymarvin.com>
2022-10-13 21:33:20 +0530

    fix: fixed data populate independently

--- code.tsx
+++ code.tsx
@@ -14,6 +14,9 @@ interface Note {
   comment: string;
   id: string;
   key: string;
+  labels: {
+    data: [];
+  };
 }
 
 function Widget() {
@@ -24,15 +27,20 @@ function Widget() {
     comment: "",
     id: "",
     key: "",
+    labels: {
+      data: [
+        {
+          text: "",
+        },
+      ],
+    },
   });
 
   const [showMore, setShowMore] = useSyncedState("open-comment", {});
 
   const widgetId = useWidgetId();
 
-  console.log("open", open);
-
-  return open ? (
+  return open || !note.id ? (
     <AutoLayout
       verticalAlignItems="center"
       cornerRadius={8}
@@ -66,28 +74,51 @@ function Widget() {
                 figma.notify("Fetching notes...");
                 setOpen(false);
 
-                let column = 0;
-                let row = 0;
+                // const originalNode = figma.getNodeById(widgetId) as WidgetNode;
+                // let currentY = originalNode.y;
+                // let defaultX = originalNode.x + originalNode.width + 10;
+                // let currentX = defaultX;
 
-                for (let i = 0; i < msg.notes.length; i++) {
-                  const originalNode = figma.getNodeById(
-                    widgetId
-                  ) as WidgetNode;
-                  const clonedWidget = originalNode.cloneWidget({
-                    note: msg.notes[i],
-                  });
+                // for (let i = 1; i <= msg.notes.length; i++) {
+                //   const clonedWidget = originalNode.cloneWidget({
+                //     note: msg.notes[i - 1],
+                //   });
 
-                  if (row === 5) {
-                    row = 0;
-                    column = column + 1;
-                    clonedWidget.x = row * 1000;
-                    clonedWidget.y = column * 1000;
-                  } else {
-                    clonedWidget.x = row * 1000;
-                    row = row + 1;
-                    clonedWidget.y = column * 1000;
-                  }
+                //   clonedWidget.x = currentX;
+                //   clonedWidget.y = currentY;
+
+                //   if (i % 5 === 0) {
+                //     currentY = currentY + 400 + 40;
+                //     currentX = defaultX;
+                //   } else {
+                //     currentX = currentX + 800 + 40;
+                //   }
+                // }
+
+                const chunkIndex: any = [[], [], [], [], []];
+                for (let i = 0; i < msg.notes.length; i += 1) {
+                  chunkIndex[i % 5].push(i);
                 }
+
+                const originalNode = figma.getNodeById(widgetId) as WidgetNode;
+                let currentY = originalNode.y;
+                let currentX = originalNode.x + originalNode.width + 10;
+
+                for (let i = 0; i < chunkIndex.length; i++) {
+                  let defaultY = currentY + chunkIndex[i].length * 400;
+                  for (let j = chunkIndex[i].length - 1; j >= 0; j -= 1) {
+                    const clonedWidget = originalNode.cloneWidget({
+                      note: msg.notes[chunkIndex[i][j]],
+                    });
+
+                    clonedWidget.x = currentX;
+                    clonedWidget.y = defaultY + 40;
+
+                    defaultY = defaultY - 400 - 40;
+                  }
+                  currentX = currentX + 800 + 40;
+                }
+
                 figma.closePlugin();
               }
               if (msg.type === "close") {
@@ -109,113 +140,120 @@ function Widget() {
     </AutoLayout>
   ) : (
     <AutoLayout
-      cornerRadius={8}
-      spacing={80}
+      cornerRadius={16}
+      spacing={48}
       direction="vertical"
-      name="Notes"
-      fill={"#ffffff"}
+      width={800}
+      height={
+        showMore[note.id as keyof object]
+          ? note.comment.length < 600
+            ? 400
+            : note.comment.length < 1000
+            ? 600
+            : note.comment.length < 10000
+            ? 1000
+            : "hug-contents"
+          : 400
+      }
+      strokeWidth={2}
+      fill={"#f7f8fa"}
+      hoverStyle={{ fill: "#ffffff" }}
+      stroke="#dce0e6"
+      strokeAlign="center"
+      key={note.id}
     >
       <AutoLayout
-        cornerRadius={16}
-        spacing={48}
-        direction="vertical"
-        width={800}
-        height={
-          showMore[note.id as keyof object]
-            ? note.comment.length < 600
-              ? 400
-              : note.comment.length < 1000
-              ? 600
-              : note.comment.length < 10000
-              ? 1000
-              : "hug-contents"
-            : 400
+        padding={
+          note.comment.length < 300
+            ? {
+                horizontal: 48,
+                vertical: 16,
+              }
+            : {
+                horizontal: 24,
+                vertical: 8,
+              }
         }
-        strokeWidth={2}
-        fill={"#f7f8fa"}
-        hoverStyle={{ stroke: "#dce0e6", fill: "#fb3569" }}
-        stroke="#dce0e6"
-        strokeAlign="center"
-        key={note.id}
-        name="a"
+        spacing={24}
+        width={"fill-parent"}
+        height={"fill-parent"}
+        direction="vertical"
       >
+        <AutoLayout width={"fill-parent"} spacing={8} direction="horizontal">
+          {note.labels.data.map((label) => (
+            <Text
+              fontSize={20}
+              italic
+              fill={"#fb3569"}
+              key={label.text}
+              // stroke="#fff"
+              // strokeAlign="center"
+              // strokeWidth={1}
+            >
+              {label.text}
+            </Text>
+          ))}
+        </AutoLayout>
+
+        <Text fontSize={32} width={"fill-parent"}>
+          {note.title}
+        </Text>
         <AutoLayout
-          padding={
-            note.comment.length < 300
-              ? {
-                  horizontal: 48,
-                  vertical: 16,
-                }
-              : {
-                  horizontal: 24,
-                  vertical: 8,
-                }
-          }
-          spacing={24}
+          padding={{
+            horizontal: 4,
+            vertical: 0,
+          }}
           width={"fill-parent"}
           height={"fill-parent"}
           direction="vertical"
+          spacing={8}
         >
-          <Text fontSize={32} width={"fill-parent"}>
-            {note.title}
-          </Text>
-          <AutoLayout
-            padding={{
-              horizontal: 4,
-              vertical: 0,
-            }}
-            width={"fill-parent"}
-            height={"fill-parent"}
-            direction="vertical"
-            spacing={8}
-          >
-            <Text
-              fontSize={
-                showMore[note.id as keyof object]
-                  ? note.comment.length < 600
-                    ? 20
-                    : note.comment.length < 1000
-                    ? 16
-                    : note.comment.length < 10000
-                    ? 12
-                    : 10
-                  : 20
-              }
-              width={"fill-parent"}
-            >
-              {showMore[note.id as keyof object]
-                ? note.comment
-                : note.comment.slice(0, 501)}
-            </Text>
-            <Text
-              onClick={() => {
-                setShowMore((comment) =>
-                  comment[note.id as keyof object]
-                    ? { ...comment, [note.id]: false }
-                    : { ...comment, [note.id]: true }
-                );
-              }}
-              fill={"#fb3569"}
-            >
-              {note.comment.length > 500
-                ? showMore[note.id as keyof object]
-                  ? "Show Less"
-                  : "Show More"
-                : ""}
-            </Text>
-          </AutoLayout>
           <Text
-            href={`https://app.heymarvin.com/annotation_tool/event/${note.key}`}
-            italic
-            fill={"#007bff"}
-            fontWeight="bold"
-            fontSize={16}
-            horizontalAlignText={"center"}
+            fontSize={
+              showMore[note.id as keyof object]
+                ? note.comment.length < 600
+                  ? 20
+                  : note.comment.length < 1000
+                  ? 16
+                  : note.comment.length < 10000
+                  ? 12
+                  : 10
+                : 20
+            }
             width={"fill-parent"}
           >
-            Link to Note
+            {showMore[note.id as keyof object]
+              ? note.comment
+              : note.comment.slice(0, 501)}
+          </Text>
+          <Text
+            onClick={() => {
+              setShowMore((comment) =>
+                comment[note.id as keyof object]
+                  ? { ...comment, [note.id]: false }
+                  : { ...comment, [note.id]: true }
+              );
+            }}
+            fill={"#fb3569"}
+          >
+            {note.comment.length > 500
+              ? showMore[note.id as keyof object]
+                ? "Show Less"
+                : "Show More"
+              : ""}
           </Text>
         </AutoLayout>
+        <Text
+          href={`https://app.heymarvin.com/annotation_tool/event/${note.key}`}
+          italic
+          fill={"#007bff"}
+          fontWeight="bold"
+          fontSize={16}
+          horizontalAlignText={"center"}
+          width={"fill-parent"}
+        >
+          Link to Note
+        </Text>
       </AutoLayout>
     </AutoLayout>
   );
